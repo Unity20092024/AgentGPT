c70743370102289ea8a688874419eba375e8507e
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:01 -0500

    improve

--- next/src/hooks/useAuth.ts
+++ next/src/hooks/useAuth.ts
@@ -1,25 +1,23 @@
 import { useRouter } from "next/router";
-import type { Session } from "next-auth";
+import type { Session, User } from "next-auth";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect } from "react";
 
 type Provider = "google" | "github" | "discord";
 
+interface UseAuthOptions {
+  protectedRoute?: boolean;
+  isAllowed?: (user: User | null) => boolean;
+}
+
 interface Auth {
-  signIn: (provider?: Provider) => Promise<void>;
-  signOut: () => Promise<void>;
+  signIn: (provider?: Provider, returnUrl?: string) => Promise<void>;
+  signOut: (redirect?: boolean) => Promise<void>;
   status: "authenticated" | "unauthenticated" | "loading";
   session: Session | null;
 }
 
-interface UseAuthOptions {
-  protectedRoute?: boolean;
-  isAllowed?: (user: Session) => boolean;
-}
-
-export function useAuth(
-  { protectedRoute, isAllowed }: UseAuthOptions = { protectedRoute: false, isAllowed: () => true }
-): Auth {
+export function useAuth({ protectedRoute, isAllowed }: UseAuthOptions = { protectedRoute: false, isAllowed: () => true }): Auth {
   const { data: session, status } = useSession();
   const { push } = useRouter();
 
@@ -28,19 +26,19 @@ export function useAuth(
       handleSignIn().catch(console.error);
     }
 
-    if (protectedRoute && status === "authenticated" && isAllowed && !isAllowed(session)) {
+    if (protectedRoute && status === "authenticated" && isAllowed && !isAllowed(session?.user)) {
       void push("/404").catch(console.error);
     }
   }, [protectedRoute, isAllowed, status, session, push]);
 
-  const handleSignIn = async () => {
-    await signIn();
+  const handleSignIn = async (provider?: Provider, returnUrl?: string) => {
+    await signIn(provider, { returnUrl });
   };
 
-  const handleSignOut = async () => {
+  const handleSignOut = async (redirect: boolean = true) => {
     await signOut({
-      callbackUrl: "/",
-    }).catch();
+      callbackUrl: redirect ? "/" : undefined,
+    }).catch(console.error);
   };
 
   return {
