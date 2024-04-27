ee9e1e9cde9b4de4df688cdfec4f5c5c10561a91
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:18 -0500

    improve

--- platform/reworkd_platform/web/api/agent/model_factory.py
+++ platform/reworkd_platform/web/api/agent/model_factory.py
@@ -7,7 +7,6 @@ from reworkd_platform.schemas.agent import LLM_Model, ModelSettings
 from reworkd_platform.schemas.user import UserBase
 from reworkd_platform.settings import Settings
 
-
 class WrappedChatOpenAI(ChatOpenAI):
     client: Any = Field(
         default=None,
@@ -15,16 +14,14 @@ class WrappedChatOpenAI(ChatOpenAI):
     )
     max_tokens: int
     model_name: LLM_Model = Field(alias="model")
-
+    model_kwargs: dict
 
 class WrappedAzureChatOpenAI(AzureChatOpenAI, WrappedChatOpenAI):
     openai_api_base: str
     openai_api_version: str
     deployment_name: str
 
-
 WrappedChat = Union[WrappedAzureChatOpenAI, WrappedChatOpenAI]
-
 
 def create_model(
     settings: Settings,
@@ -38,36 +35,43 @@ def create_model(
     )
 
     llm_model = force_model or model_settings.model
-    model: Type[WrappedChat] = WrappedChatOpenAI
-    base, headers, use_helicone = get_base_and_headers(settings, model_settings, user)
-    kwargs = {
-        "openai_api_base": base,
-        "openai_api_key": model_settings.custom_api_key or settings.openai_api_key,
-        "temperature": model_settings.temperature,
-        "model": llm_model,
-        "max_tokens": model_settings.max_tokens,
-        "streaming": streaming,
-        "max_retries": 5,
-        "model_kwargs": {"user": user.email, "headers": headers},
-    }
-
+    model: Type[WrappedChat]
     if use_azure:
         model = WrappedAzureChatOpenAI
         deployment_name = llm_model.replace(".", "")
-        kwargs.update(
-            {
-                "openai_api_version": settings.openai_api_version,
-                "deployment_name": deployment_name,
-                "openai_api_type": "azure",
-                "openai_api_base": base.rstrip("v1"),
-            }
-        )
+        base = settings.openai_api_base.rstrip("v1")
+        kwargs = {
+            "openai_api_version": settings.openai_api_version,
+            "deployment_name": deployment_name,
+            "openai_api_type": "azure",
+            "openai_api_base": base,
+            "model": deployment_name,
+            "max_tokens": model_settings.max_tokens,
+            "temperature": model_settings.temperature,
+            "streaming": streaming,
+            "max_retries": 5,
+            "model_kwargs": {
+                "user": user.email,
+                "headers": get_headers(settings, user, use_helicone=True),
+            },
+        }
+    else:
+        model = WrappedChatOpenAI
+        kwargs = {
+            "openai_api_base": settings.openai_api_base,
+            "openai_api_key": model_settings.custom_api_key or settings.openai_api_key,
+            "temperature": model_settings.temperature,
+            "model": llm_model,
+            "max_tokens": model_settings.max_tokens,
+            "streaming": streaming,
+            "max_retries": 5,
+            "model_kwargs": {
+                "user": user.email,
+                "headers": get_headers(settings, user),
+            },
+        }
 
-        if use_helicone:
-            kwargs["model"] = deployment_name
-
-    return model(**kwargs)  # type: ignore
-
+    return model(**kwargs)
 
 def get_base_and_headers(
     settings_: Settings, model_settings: ModelSettings, user: UserBase
@@ -94,4 +98,18 @@ def get_base_and_headers(
         else None
     )
 
-    return base, headers, use_helicone
+    return base, headers
+
+def get_headers(settings: Settings, user: UserBase, use_helicone: bool = False) -> dict:
+    headers = {
+        "Authorization": f"Bearer {settings.openai_api_key}",
+        "User-Email": user.email,
+    }
+    if use_helicone:
+        headers.update({
+            "Helicone-Auth": f"Bearer {settings.helicone_api_key}",
+            "Helicone-Cache-Enabled": "true",
+            "Helicone-User-Id": user.id,
+            "Helicone-OpenAI-Api-Base": settings.openai_api_base,
+        })
+    return headers
