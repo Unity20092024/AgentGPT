2287ae04fcdb942005daf201ad86c941e375a711
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:51 -0500

    improve

--- next/jest.config.cjs
+++ next/jest.config.cjs
@@ -13,7 +13,18 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  // Add any additional Jest configurations here
+  coverageReporters: ["text", "clover", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
+
