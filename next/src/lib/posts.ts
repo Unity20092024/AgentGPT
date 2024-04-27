cf571ff4d044c71898c08385a7bac16abc0ce948
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:58 -0500

    improve

--- next/src/lib/posts.ts
+++ next/src/lib/posts.ts
@@ -18,17 +18,27 @@ export function getSortedPostsData(): SlugData[] {
   const allPostsData: SlugData[] = fileNames.map((fileName) => {
     const id = fileName.replace(/\.mdx$/, "");
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-    const matterResult = matter(fileContents);
 
-    return {
-      id,
-      date: matterResult.data.date as string,
-      ...matterResult.data,
-    };
+    try {
+      const fileContents = fs.readFileSync(fullPath, "utf8");
+      const matterResult = matter(fileContents);
+
+      return {
+        id,
+        date: matterResult.data?.date || "",
+        ...matterResult.data,
+      };
+    } catch (error) {
+      console.error(`Error reading file ${fullPath}:`, error);
+      return { id };
+    }
   });
 
   return allPostsData.sort((a, b) => {
+    if (!a.date || !b.date) {
+      return 0;
+    }
+
     if (a.date < b.date) {
       return 1;
     } else {
@@ -46,12 +56,18 @@ export interface PostData {
 
 export function getPostData(slug: string): PostData {
   const fullPath = path.join(postsDirectory, `${slug}.mdx`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  const matterResult = matter(fileContents);
 
-  return {
-    slug,
-    ...matterResult.data,
-    content: matterResult.content,
-  };
+  try {
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const matterResult = matter(fileContents);
+
+    return {
+      slug,
+      ...matterResult.data,
+      content: matterResult.content,
+    };
+  } catch (error) {
+    console.error(`Error reading file ${fullPath}:`, error);
+    return { slug };
+  }
 }
