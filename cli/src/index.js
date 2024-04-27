ee419dbfe2895b3c2908bc38ae474fc55709d12b
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:51 -0500

    improve

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,40 +1,58 @@
 import inquirer from "inquirer";
 import dotenv from "dotenv";
 import { printTitle } from "./helpers.js";
-import { doesEnvFileExist, generateEnv, testEnvFile } from "./envGenerator.js";
+import {
+  doesEnvFileExist,
+  generateEnv,
+  testEnvFile,
+} from "./envGenerator.js";
 import { newEnvQuestions } from "./questions/newEnvQuestions.js";
 import { existingEnvQuestions } from "./questions/existingEnvQuestions.js";
 import { spawn } from "child_process";
 import chalk from "chalk";
 
-const handleExistingEnv = () => {
+const handleExistingEnv = async () => {
   console.log(chalk.yellow("Existing ./next/env file found. Validating..."));
 
   try {
-    testEnvFile();
+    await testEnvFile();
   } catch (e) {
-    console.log(e.message);
+    console.error(e.message);
     return;
   }
 
-  inquirer.prompt(existingEnvQuestions).then((answers) => {
-    handleRunOption(answers.runOption);
-  });
+  const answers = await inquirer.prompt(existingEnvQuestions);
+  handleRunOption(answers.runOption);
 };
 
-const handleNewEnv = () => {
-  inquirer.prompt(newEnvQuestions).then((answers) => {
+const handleNewEnv = async () => {
+  if (process.env.NODE_ENV !== "development") {
+    console.error("Env file can only be created in development mode.");
+    process.exit(1);
+  }
+
+  const answers = await inquirer.prompt(newEnvQuestions);
+
+  try {
     dotenv.config({ path: "./.env" });
     generateEnv(answers);
     console.log("\nEnv files successfully created!");
-    handleRunOption(answers.runOption);
-  });
+  } catch (e) {
+    console.error("Error creating env files:", e.message);
+    process.exit(1);
+  }
+
+  handleRunOption(answers.runOption);
 };
 
 const handleRunOption = (runOption) => {
   if (runOption === "docker-compose") {
     const dockerComposeUp = spawn("docker-compose", ["up", "--build"], {
       stdio: "inherit",
+    });
+
+    dockerComposeUp.on("error", (error) => {
+      console.error(`Error running docker-compose: ${error.message}`);
     });
   }
 
