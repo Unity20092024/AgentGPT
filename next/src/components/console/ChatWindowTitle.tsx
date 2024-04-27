88c8f025ea286a2980b3335220ea7be3c26a88f8
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:56 -0500

    improve

--- next/src/components/console/ChatWindowTitle.tsx
+++ next/src/components/console/ChatWindowTitle.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 
-import type { GPTModelNames } from "../../types";
-import { GPT_35_TURBO_16K, GPT_4 } from "../../types";
+type GPTModelNames = "GPT-3.5" | "GPT-3.5-16K" | "GPT-4";
 
-export const ChatWindowTitle = ({ model }: { model: GPTModelNames }) => {
-  if (model === GPT_4) {
-    return (
-      <>
-        Agent<span className="text-amber-500">GPT-4</span>
-      </>
-    );
-  }
+export const ChatWindowTitle: React.FC<{ model: GPTModelNames }> = ({ model }) => {
+  const getModelName = () => {
+    if (model === "GPT-4") {
+      return <span className="text-amber-500">GPT-4</span>;
+    }
 
-  if (model === GPT_35_TURBO_16K) {
-    return (
-      <>
-        Agent
+    if (model === "GPT-3.5-16K") {
+      return (
         <span className="text-neutral-400">
           GPT-3.5<span className="text-amber-500">-16K</span>
         </span>
-      </>
-    );
-  }
+      );
+    }
+
+    return <span className="text-neutral-400">GPT-3.5</span>;
+  };
 
   return (
     <>
-      Agent<span className="text-neutral-400">GPT-3.5</span>
+      Agent {getModelName()}
     </>
   );
 };
