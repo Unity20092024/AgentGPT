6e77fefc3e780973f9fede2093feff145924d799
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:00 -0500

    improve

--- next/src/types/index.ts
+++ next/src/types/index.ts
@@ -1,2 +1,13 @@
+// index.ts
+
 export * from "./propTypes";
 export * from "./modelSettings";
+
+// propTypes.ts
+
+export * as PropTypesFromReact from "react";
+
+// modelSettings.ts
+
+export * as ModelSettingsFromConfigFile from "./configFile";
+
