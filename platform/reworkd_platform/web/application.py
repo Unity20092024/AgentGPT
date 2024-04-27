d4b006b5b211f6f847430c2edf9811e76464da92
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:12 -0500

    improve

--- platform/reworkd_platform/web/application.py
+++ platform/reworkd_platform/web/application.py
@@ -1,28 +1,19 @@
 from importlib import metadata
+from typing import Any
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import UJSONResponse
-
 from reworkd_platform.logging import configure_logging
 from reworkd_platform.settings import settings
 from reworkd_platform.web.api.error_handling import platformatic_exception_handler
 from reworkd_platform.web.api.errors import PlatformaticError
 from reworkd_platform.web.api.router import api_router
-from reworkd_platform.web.lifetime import (
-    register_shutdown_event,
-    register_startup_event,
-)
+from reworkd_platform.web.lifetime import register_shutdown_event, register_startup_event
 
 
 def get_app() -> FastAPI:
-    """
-    Get FastAPI application.
-
-    This is the main constructor of an application.
-
-    :return: application.
-    """
+    """Get FastAPI application."""
     configure_logging()
 
     app = FastAPI(
@@ -43,12 +34,10 @@ def get_app() -> FastAPI:
         allow_headers=["*"],
     )
 
-    # Adds startup and shutdown events.
     register_startup_event(app)
     register_shutdown_event(app)
 
-    # Main router for the API.
-    app.include_router(router=api_router, prefix="/api")
+    app.include_router(api_router, prefix="/api")
 
     app.exception_handler(PlatformaticError)(platformatic_exception_handler)
 
