cae1e32777daa9a9f555c4001b0a1f4b8f389bad
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:21 -0500

    improve

--- next/src/services/agent/agent-work/start-task-work.ts
+++ next/src/services/agent/agent-work/start-task-work.ts
@@ -2,26 +2,32 @@ import type AgentWork from "./agent-work";
 import type AutonomousAgent from "../autonomous-agent";
 
 export default class StartGoalWork implements AgentWork {
-  tasksValues: string[] = [];
+  private tasksValues: string[] = [];
+  private goalMessage?: string;
 
   constructor(private parent: AutonomousAgent) {}
 
-  run = async () => {
-    const goalMessage = this.parent.messageService.sendGoalMessage(this.parent.model.getGoal());
+  async run(): Promise<void> {
+    this.goalMessage = this.parent.messageService.sendGoalMessage(this.parent.model.getGoal());
     this.tasksValues = await this.parent.api.getInitialTasks();
     await this.parent.api.createAgent();
-    this.parent.api.saveMessages([goalMessage]);
-  };
+  }
 
-  conclude = async () => {
-    const messages = await this.parent.createTaskMessages(this.tasksValues);
-    this.parent.api.saveMessages(messages);
-  };
+  async conclude(): Promise<void> {
+    if (!this.goalMessage) {
+      throw new Error("goalMessage is not defined");
+    }
 
-  onError = (e: unknown): boolean => {
+    const taskMessages = this.parent.createTaskMessages(this.tasksValues);
+    this.parent.api.saveMessages([this.goalMessage, ...taskMessages]);
+  }
+
+  onError(e: unknown): boolean {
     this.parent.messageService.sendErrorMessage(e);
     return true;
-  };
+  }
 
-  next = () => undefined;
+  next(): undefined {
+    return undefined;
+  }
 }
