5d51165c67df8db5d60e16eba310b3a6aea788e4
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:58 -0500

    improve

--- next/src/components/landing/Backing.tsx
+++ next/src/components/landing/Backing.tsx
@@ -1,44 +1,60 @@
-import clsx from "clsx";
+import React from "react";
 
-import PanacheLogo from "../../../public/logos/panache-default-solid.svg";
-import YCLogo from "../../../public/logos/yc-default-solid.svg";
+const Logos = () => (
+  <div className="flex flex-row items-center gap-x-1">
+    <a
+      className="z-10 -mr-2 cursor-pointer"
+      href="https://www.ycombinator.com/companies/reworkd"
+      target="_blank"
+      title="Y Combinator"
+    >
+      <img src="../../../public/logos/yc-default-solid.svg" alt="Y Combinator logo" />
+    </a>
+    <a
+      href="https://www.panache.vc/"
+      target="_blank"
+      title="Panache Ventures"
+    >
+      <img src="../../../public/logos/panache-default-solid.svg" alt="Panache Ventures logo" />
+    </a>
+  </div>
+);
 
-const Backing = (props: { className?: string }) => (
+const Backing = (props) => (
   <div
-    className={clsx(
-      "flex flex-col font-inter text-xs font-normal text-white/50 md:text-sm",
+    className={[
+      "flex",
+      "flex-col",
+      "font-inter",
+      "text-xs",
+      "font-normal",
+      "text-white/50",
+      "md:text-sm",
       props.className
-    )}
+    ].join(" ")}
   >
     <div className="flex flex-row items-center gap-x-1">
       <div className="ml-2 mr-1 flex flex-row items-center">
+        <Logos />
+      </div>
+      <div className="hidden tracking-wide sm:flex">Backed By</div>
+      <div className="flex flex-row items-center gap-1 font-light text-white/95">
         <a
-          className=" z-10 -mr-2 cursor-pointer"
+          className="cursor-pointer"
           href="https://www.ycombinator.com/companies/reworkd"
           target="_blank"
         >
-          <YCLogo />
+          Y Combinator
         </a>
-        <a href="https://www.panache.vc/" target="_blank" className="cursor-pointer">
-          <PanacheLogo />
+        <span>and</span>
+        <a
+          className="cursor-pointer"
+          href="https://www.panache.vc/"
+          target="_blank"
+        >
+          Panache Ventures
         </a>
       </div>
-      <div className="hidden tracking-wide sm:flex">Backed By</div>
-      <a
-        className="flex cursor-pointer flex-row items-center gap-1 font-light text-white/95"
-        href="https://www.ycombinator.com/companies/reworkd"
-        target="_blank"
-      >
-        Y Combinator
-      </a>
-      <span>and</span>
-      <a
-        className="cursor-pointer font-light text-white/95"
-        href="https://www.panache.vc/"
-        target="_blank"
-      >
-        Panache Ventures
-      </a>
     </div>
   </div>
 );
