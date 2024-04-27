ea084f0708ff6d767f468cff654c969858fc3fdd
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:47 -0500

    improve

--- cli/src/questions/existingEnvQuestions.js
+++ cli/src/questions/existingEnvQuestions.js
@@ -1,5 +1,12 @@
+// sharedQuestions.js
+export const RUN_OPTION_QUESTION = {
+  // define your question here
+};
+
+// existingEnvQuestions.js
 import { RUN_OPTION_QUESTION } from "./sharedQuestions.js";
 
 export const existingEnvQuestions = [
-  RUN_OPTION_QUESTION
+  RUN_OPTION_QUESTION,
+  // add more questions here if needed
 ];
