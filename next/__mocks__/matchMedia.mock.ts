0c4087a1cd9e84bff1414873678ab7eda26f0d5b
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:50 -0500

    improve

--- next/__mocks__/matchMedia.mock.ts
+++ next/__mocks__/matchMedia.mock.ts
@@ -1,14 +1,16 @@
-// When using the matchMedia API in your tests, you will need to mock it.
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation((query: string) => ({
+const createMatchMediaMock = () => {
+  return jest.fn().mockImplementation((query: string) => ({
     matches: false,
     media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
-  })),
+  }));
+};
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: createMatchMediaMock(),
 });
