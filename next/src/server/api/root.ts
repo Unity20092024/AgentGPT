7b5b81b4acf4afd7a5310ce8d8bf0dce8cc13823
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:00 -0500

    improve

--- next/src/server/api/root.ts
+++ next/src/server/api/root.ts
@@ -1,9 +1,46 @@
-import { agentRouter } from "./routers/agentRouter";
-import { createTRPCRouter } from "./trpc";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { z } from "zod";
+import { agentService } from "../services/agentService";
 
-export const appRouter = createTRPCRouter({
-  agent: agentRouter,
+const agentRouter = createTRPCRouter({
+  getAgents: publicProcedure.query(() => {
+    return agentService.getAgents();
+  }),
+
+  getAgentById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ input }) => {
+      return agentService.getAgentById(input.id);
+    }),
+
+  createAgent: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        email: z.string().email(),
+      })
+    )
+    .mutation(({ input }) => {
+      return agentService.createAgent(input);
+    }),
+
+  updateAgent: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        email: z.string().email(),
+      })
+    )
+    .mutation(({ input }) => {
+      return agentService.updateAgent(input);
+    }),
+
+  deleteAgent: publicProcedure.input(z.object({ id: z.string() })).mutation(
+    ({ input }) => {
+      return agentService.deleteAgent(input.id);
+    }
+  ),
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
