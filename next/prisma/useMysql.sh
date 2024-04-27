77c22954674fe15e127f61cd0c8e506671bd1927
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:50 -0500

    improve

--- next/prisma/useMysql.sh
+++ next/prisma/useMysql.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-cd "$(dirname "$0")" || exit 1
 
-rm schema.prisma
-mv schema.prisma.mysql schema.prisma
+# Get the absolute path of the current script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Change to the script directory
+cd "${SCRIPT_DIR}" || exit 1
+
+# Check if the source file exists before attempting to move it
+if [ -f schema.prisma.mysql ]; then
+  # Rename the source file to schema.prisma
+  mv schema.prisma.mysql schema.prisma
+else
+  echo "Source file schema.prisma.mysql not found!"
+fi
 
