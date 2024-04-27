e5721ad54bc64775404c1b91da600b04ecf1e13d
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:12 -0500

    improve

--- platform/reworkd_platform/tests/test_dependancies.py
+++ platform/reworkd_platform/tests/test_dependancies.py
@@ -1,7 +1,10 @@
 import pytest
+from unittest.mock import Mock
 
+import pytest_asyncio
 from reworkd_platform.web.api.agent import dependancies
 
+pytest_asyncio.patch("reworkd_platform.web.api.agent.crud.create_task", new_callable=AsyncMock)(mocker)
 
 @pytest.mark.anyio
 @pytest.mark.parametrize(
@@ -15,12 +18,12 @@ from reworkd_platform.web.api.agent import dependancies
     ],
 )
 async def test_agent_validate(mocker, validator, step):
+    mocker.patch("reworkd_platform.web.api.agent.crud.create_task", new_callable=AsyncMock)
     run_id = "asim"
     crud = mocker.Mock()
-    body = mocker.Mock()
+    body = Mock()
     body.run_id = run_id
 
-    crud.create_task = mocker.AsyncMock()
-
     await validator(body, crud)
-    crud.create_task.assert_called_once_with(run_id, step)
+
+    crud.create_task.assert_called_once_with.assert_called_once_with(run_id, step)
