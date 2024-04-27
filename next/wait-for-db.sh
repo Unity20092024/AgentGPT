ff9fce527d834c02b5d870b15a5d20c1aae16dcb
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:02 -0500

    improve

--- next/wait-for-db.sh
+++ next/wait-for-db.sh
@@ -1,11 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Check if the required arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <host> <port>"
+    exit 1
+fi
 
 host="$1"
 port="$2"
 
-until echo "SELECT 1;" | nc "$host" "$port" > /dev/null 2>&1; do
-  >&2 echo "Database is unavailable - Sleeping..."
-  sleep 2
+# Use a loop with timeout to check if the database is available
+while true; do
+    if nc -z -w3 "$host" "$port"; then
+        echo "Database is available! Continuing..."
+        break
+    else
+        echo "Database is unavailable - Sleeping..."
+        sleep 5
+    fi
 done
 
->&2 echo "Database is available! Continuing..."
