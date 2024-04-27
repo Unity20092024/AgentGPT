d7d48c427afdd46784d41153a65261993d68d8a4
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:47 -0500

    improve

--- cli/src/questions/sharedQuestions.js
+++ cli/src/questions/sharedQuestions.js
@@ -1,10 +1,14 @@
-export const RUN_OPTION_QUESTION = {
+const RUN_OPTION_QUESTION = {
   type: 'list',
   name: 'runOption',
   choices: [
-    { value: "docker-compose", name: "🐋 Docker-compose (Recommended)" },
-    { value: "manual", name: "💪 Manual (Not recommended)" },
+    { value: 'docker-compose', name: '🐋 Docker-compose (Recommended)' },
+    { value: 'manual', name: '💪 Manual (Not recommended)' },
   ],
   message: 'How will you be running AgentGPT?',
-  default: "docker-compose",
-}
+  default: 'docker-compose',
+};
+
+module.exports = {
+  RUN_OPTION_QUESTION,
+};
