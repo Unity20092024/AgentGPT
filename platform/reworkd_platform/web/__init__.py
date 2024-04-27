60eadbd1d561c773343272294dfc9b7177fba731
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:27:13 -0500

    improve

--- platform/reworkd_platform/web/__init__.py
+++ platform/reworkd_platform/web/__init__.py
@@ -1,1 +1,40 @@
-"""WEB API for reworkd_platform."""
+"""WEB API for reworked_platform."""
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/platform', methods=['GET'])
+def get\_platform():
+    """Get information about the platform."""
+    # Here you can add the logic to retrieve information about the platform
+    platform_info = {"name": "reworked_platform", "version": "1.0.0"}
+    return jsonify(platform_info)
+
+@app.route('/api/platform', methods=['POST'])
+def create\_platform():
+    """Create a new platform."""
+    # Here you can add the logic to create a new platform
+    return jsonify({"message": "Platform created"}), 201
+
+@app.route('/api/platform/<string:platform_id>', methods=['GET'])
+def get\_platform\_by\_id(platform_id):
+    """Get information about a specific platform by id."""
+    # Here you can add the logic to retrieve information about a specific platform by id
+    platform_info = {"id": platform_id, "name": "reworked\_platform", "version": "1.0.0"}
+    return jsonify(platform_info)
+
+@app.route('/api/platform/<string:platform_id>', methods=['PUT'])
+def update\_platform\_by\_id(platform_id):
+    """Update a specific platform by id."""
+    # Here you can add the logic to update a specific platform by id
+    return jsonify({"message": "Platform updated"})
+
+@app.route('/api/platform/<string:platform_id>', methods=['DELETE'])
+def delete\_platform\_by\_id(platform_id):
+    """Delete a specific platform by id."""
+    # Here you can add the logic to delete a specific platform by id
+    return jsonify({"message": "Platform deleted"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
