726cc12b2b98e42ff0cb0f89d6d8f541c01abeb0
Unity20092024 <143305940+Unity20092024@users.noreply.github.com>
2024-04-26 21:26:58 -0500

    improve

--- next/src/components/BannerBadge.tsx
+++ next/src/components/BannerBadge.tsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren, AnchorHTMLAttributes } from "react";
 import React from "react";
 import { FaChevronRight } from "react-icons/fa";
 
-type BadgeProps = PropsWithChildren<React.AnchorHTMLAttributes<HTMLAnchorElement>>;
+type BannerBadgeProps = PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>> & {
+  title?: string;
+};
 
-const BannerBadge = ({ children, className, ...props }: BadgeProps) => (
+const BannerBadge = ({ children, className, title, href, tabIndex, onKeyDown, ...props }: BannerBadgeProps) => (
   <div
     className={clsx(
       "rounded-full bg-gradient-to-tl from-[#A02BFE] via-[#02FCF1] to-[#A02BFE] p-[1px] subpixel-antialiased",
@@ -14,6 +16,11 @@ const BannerBadge = ({ children, className, ...props }: BadgeProps) => (
   >
     <a
       className="animate-border-pulse py group relative flex w-max cursor-pointer items-center gap-2 rounded-full bg-black px-4 py-2 text-xs text-white"
+      href={href}
+      role="button"
+      tabIndex={tabIndex}
+      onKeyDown={onKeyDown}
+      title={title}
       {...props}
     >
       <span>{children}</span>
